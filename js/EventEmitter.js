39c6597929f51d9bb615112be7653cab4ddf405f
EEvgeniiF <ejackf0606@gmail.com>
2016-11-08 22:05:10 +0300

    edit some signature, add comments

--- js/EventEmitter.js
+++ js/EventEmitter.js
@@ -3,13 +3,17 @@ export default class EventEmitter {
 	listeners = {};
 	observables = {};
 
+	/*
+		methods for observable object
+	 */
+	// subscribe to event
 	on(eventName, handler) {
 		if (typeof this.listeners[eventName] === 'undefined') {
 			this.listeners[eventName] = [];
 		}
 		this.listeners[eventName].push(handler);
 	}
-
+	// rise the event
 	emit(eventName, ...args) {
 		if (typeof this.listeners[eventName] !== 'undefined') {
 			this.listeners[eventName].forEach(func => {
@@ -17,43 +21,45 @@ export default class EventEmitter {
 			});
 		}
 	}
-
+	//unsubscribe from event
 	off(eventName, handler) {
 		if (typeof this.listeners[eventName] !== 'undefined') {
 			this.listeners[eventName] = this.listeners[eventName].filter(listen => handler != listen);
 		}
 	}
-
+	//remove all subscribers
 	removeAll(eventName) {
 		delete this.listeners[eventName];
 	}
 
-	listenTo(observable, eventName) {
+	/*
+		methods for listener object
+	 */
+
+	// connect handler method to listen eventName of observable object
+	listenTo(observable, eventName, handler) {
 		if (typeof observable.on === 'undefined' ||
 			typeof observable.off === 'undefined') {
 			throw new Error('There are not "on" or "off" methods');
 		}
-		observable.on(eventName, this.handler);
+		observable.on(eventName, handler);
 		if (typeof this.observables[eventName] === 'undefined') {
 			this.observables[eventName] = [];
 		}
-		this.observables[eventName].push(observable);
+		this.observables[eventName].push({observable, handler});
 
-	}
-
-	handler(eventName, ...args) {
 	}
 
 	unlistenFrom(observable, eventName) {
-		observable.off(eventName, this.handler);
+		observable.off(eventName, this.observables[eventName].handler);
 		if (typeof this.observables[eventName] !== 'undefined') {
-			this.observables[eventName] = this.observables[eventName].filter(obs => obs !== observable);
+			this.observables[eventName] = this.observables[eventName].filter(obs => obs.observable !== observable);
 		}
 	}
 
 	unlistenAll() {
 		for (let key in this.observables) {
-			this.observables[key].forEach(obs => obs.off(key, this.handler));
+			this.observables[key].forEach(obs => obs.observable.off(key, obs.handler));
 			delete this.observables[key];
 		}
 	}
