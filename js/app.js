43b58f1d15f376ec49302965a0362174f6d9c6f7
MaximPestryakov <maximpestryakov@gmail.com>
2016-11-25 14:31:37 +0300

    Works 2 todo lists

--- js/app.js
+++ js/app.js
@@ -2,4 +2,5 @@
 
 const TodoCollection = require('./collections/todo');
 const AppView = require('./views/app');
-new AppView(document, new TodoCollection());
+new AppView(document.querySelector('section#todoapp_1'), new TodoCollection());
+new AppView(document.querySelector('section#todoapp_2'), new TodoCollection());
