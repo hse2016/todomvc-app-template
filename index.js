0ce2f3c51bcc2d3200a1244ee06d82362b58b9cc
Artem Maksimov <temamaksimov@gmail.com>
2016-11-27 04:01:22 +0300

    Fix express routing

--- index.js
+++ index.js
@@ -5,14 +5,11 @@
 const express = require('express');
 
 const app = express();
-app.use(express.logger());
+// app.use(express.logger());
 
-app.use(express.static('css'));
-app.use(express.static('js'));
-
-app.get('/', (request, response) => {
-	response.render('index.html');
-});
+app.use('/', express.static(`${__dirname}/`));
+app.use('/css', express.static(`${__dirname}/css`));
+app.use('/js', express.static(`${__dirname}/js`));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
