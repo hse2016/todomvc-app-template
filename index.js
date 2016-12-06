96b62be5507126509b12a57129f31faca02a8740
DAlexIV <alexiv@bookmate.com>
2016-12-07 00:42:13 +0300

    Added script & removed default todos

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by tema on 27.11.16. :) Thanks a lot!
+ */
+
+const express = require('express');
+
+const app = express();
+
+app.use('/', express.static(`${__dirname}/`));
+app.use('/css', express.static(`${__dirname}/css`));
+app.use('/js', express.static(`${__dirname}/js`));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`Listening on ${port}`);
+});
